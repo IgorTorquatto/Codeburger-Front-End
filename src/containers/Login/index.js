3ae3e1488e2888942a66b7a3810222d309c82e57
Igor <igor.torquato.20@gmail.com>
2024-06-29 20:46:54 -0300

    add: login page

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React from 'react';
+import {
+  Container,
+  LeftContainer,
+  LoginImage,
+  RightContainer,
+  FormGroup,
+  InputLabel,
+  Input,
+  Button,
+  Register
+} from './styles';
+import LoginImg from '../../assets/9 1.svg';
 
 function Login() {
+  const focusInput = (id) => {
+    const inputElement = document.getElementById(id);
+    if (inputElement) {
+      inputElement.focus();
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Container>
+      <LeftContainer>
+        <LoginImage src={LoginImg} alt="Login Image" />
+      </LeftContainer>
+      <RightContainer>
+        <h1>Login</h1>
+        <FormGroup>
+          <InputLabel htmlFor="email" onClick={() => focusInput('email')}>
+            Email
+          </InputLabel>
+          <Input type="email" id="email" />
+        </FormGroup>
+        <FormGroup>
+          <InputLabel htmlFor="password" onClick={() => focusInput('password')}>
+            Password
+          </InputLabel>
+          <Input type="password" id="password" />
+        </FormGroup>
+        <Button>Sign In</Button>
+        <Register>
+          Don't have an account? <a href="#">Sign Up</a>
+        </Register>
+      </RightContainer>
+    </Container>
+  );
 }
 
-export default Login
-
-
+export default Login;
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    overflow-x: hidden;
+    overflow-y: hidden;
+
+    @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+export const LeftContainer = styled.div`
+    width: 50%;
+    height: 100%;
+
+    @media (max-width: 768px) {
+    width: 100%;
+    height: 50%;
+    }
+`
+
+export const LoginImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+
+export const RightContainer = styled.div`
+    width: 50%;
+    height: 100%;
+    background-color: #373737;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1{
+        color: #ffffff;
+        font-size: 2.5em;
+        margin-bottom: 40px;
+    }
+
+    @media (max-width: 768px) {
+        width: 100%;
+        height: 50%;
+
+        h1{
+            font-size: 2em;
+            margin-bottom: 30px;
+        }
+    }
+`
+
+export const FormGroup = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+
+    @media (max-width: 768px) {
+        width: 80%;
+    }
+`
+
+export const InputLabel = styled.p`
+    color: #ffffff;
+    font-size: 1.05em;
+    margin-bottom: 5px;
+
+    @media (max-width: 768px) {
+        font-size: 1em;
+    }
+`
+
+export const Input = styled.input`
+    width: 100%;
+    height: 39px;
+    border-radius: 10px;
+    outline: none;
+    border: none;
+    box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 21px;
+    padding: 15px;
+`
+
+export const Button = styled.button`
+    border: none;
+    outline: none;
+    color: #ffffff;
+    font-size: 1em;
+    background-color: #9758A6;
+    width: 30%;
+    height: 39px;
+    border-radius: 15px;
+    margin: 5px 0px 15px 0px;
+
+    &:hover{
+        cursor: pointer;
+    }
+
+`
+
+export const Register = styled.p`
+    color: #ffffff;
+
+    a{
+        color: rgba(2, 117, 216, 100);
+        text-decoration: none;
+    }
+    a:hover{
+        color: rgba(2, 117, 216, 0.8);
+        cursor: pointer;
+    }
+`
