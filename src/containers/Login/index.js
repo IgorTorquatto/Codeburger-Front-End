39d6719064281d4fedd46d1110dec671fe5f9724
Igor <igor.torquato.20@gmail.com>
2024-07-03 18:50:19 -0300

    style: organizing container

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -14,21 +14,32 @@ import Button from '../../components/Button'
 import LoginImg from '../../assets/9 1.svg';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
-import * as yup from 'yup'
+import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import api from '../../services/api'
 
 function Login() {
 
-  const navigate = useNavigate()
-  const schema = yup.object({
-    email: yup.string().email("Digite um e-mail válido").required("O e-mail é obrigatório"),
-    password: yup.string().required("A senha é obrigatória").min(6, "A senha deve ter pelo menos 6 dígitos"),
+  //Variables
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Digite um e-mail válido')
+      .required('O e-mail é obrigatório'),
+    password: Yup.string()
+      .required('A senha é obrigatória')
+      .min(6, 'A senha deve ter pelo 6 digítos')
   })
-  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+
+  const { 
+    register,
+    handleSubmit,
+    formState: { errors } } = useForm({
     resolver: yupResolver(schema)
   })
 
+  const navigate = useNavigate()
+
+  //Functions
   const onSubmit =  async clientData => {
       const response = await api.post('sessions',{
         email: clientData.email,
@@ -51,12 +62,17 @@ function Login() {
 
   return (
     <Container>
+
       <LeftContainer>
         <LoginImage src={LoginImg} alt="Login Image" />
       </LeftContainer>
+
       <RightContainer>
+
         <h1>Login</h1>
+        
         <form noValidate onSubmit={handleSubmit(onSubmit)}>
+
           <FormGroup>
             <InputLabel htmlFor="email" onClick={() => focusInput('email')}>
               Email
@@ -64,6 +80,7 @@ function Login() {
             <Input type="email" id="email" {...register("email")} error={errors.email?.message} />
             <Error>{errors.email?.message}</Error>
           </FormGroup>
+
           <FormGroup>
             <InputLabel htmlFor="password" onClick={() => focusInput('password')}>
               Senha
@@ -71,12 +88,17 @@ function Login() {
             <Input type="password" id="password" {...register("password")} error={errors.password?.message} />
             <Error>{errors.password?.message}</Error>
           </FormGroup>
+
           <Button type="submit" style={{marginTop: 5 , marginBottom: 20}}>Entrar</Button>
+
         </form>
+
         <LeftFooter>
           Não possui uma conta? <a onClick={signUp}>Cadastre-se</a>
         </LeftFooter>
+
       </RightContainer>
+
     </Container>
   );
 }
