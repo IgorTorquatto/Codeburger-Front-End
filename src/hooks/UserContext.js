f1dd4953b7e5e011f6d078e6e0f9979b21478d70
Igor <igor.torquato.20@gmail.com>
2024-07-10 17:53:04 -0300

    feat: add new hook useContext

--- src/hooks/UserContext.js
+++ src/hooks/UserContext.js
@@ -1,45 +1,44 @@
-import React, { createContext, useContext,useState,useEffect } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 const UserContext = createContext({})
 
-export const UserProvider = ({ children }) =>{
-    const [userData,setUserData] =  useState({})
+export const UserProvider = ({ children }) => {
+    const [userData, setUserData] = useState({})
 
-    const putUserData = async userInfo =>{
+    const putUserData = async userInfo => {
         setUserData(userInfo)
 
-        await localStorage.setItem('codeburger:userData',JSON.stringify(userInfo))
+        //In addition to creating the context of user data, we also record it in the browser's localstorage
+        await localStorage.setItem('codeburger:userData', JSON.stringify(userInfo))
     }
 
-
-    
-    useEffect(()=>{
+    useEffect(() => {
         //asynchronous function to capture user information that is in localStorage
-        const loadUserData = async () =>{
+        const loadUserData = async () => {
             const clientInfo = await localStorage.getItem('codeburger:userData')
 
             //If there is already something in localstorage, store it in our state
-            if(clientInfo){
+            if (clientInfo) {
                 setUserData(JSON.parse(clientInfo))
             }
 
         }
 
         loadUserData()
-    },[])
+    }, [])
 
     return (
-        <UserContext.Provider value={{putUserData,userData}}>
+        <UserContext.Provider value={{ putUserData, userData }}>
             {children}
         </UserContext.Provider>
     )
 }
 
-export const useUser = () =>{
+export const useUser = () => {
     const context = useContext(UserContext)
 
-    if(!context){
+    if (!context) {
         throw new Error("useUser must be used with UserContext")
     }
 
