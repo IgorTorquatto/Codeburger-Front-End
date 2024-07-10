5477e870118baccc264a90b90c30973bef6072cf
Igor <igor.torquato.20@gmail.com>
2024-07-10 20:07:43 -0300

    feat: add product offer carousel

---
+++ src/components/OffersCarousel/index.js
@@ -0,0 +1,55 @@
+import React, { useEffect,useState } from 'react'
+import { Container,Title,ContainerItems,Image_Carousel,Button } from './styles'
+import Title_Offers from '../../assets/OFERTAS.png'
+import api from '../../services/api'
+import Carousel from 'react-elastic-carousel'
+
+function OffersCarousel() {
+  const [offers,setOffers] = useState([])
+
+  useEffect(()=>{
+    async function loadOffers(){
+        const { data } = await api.get('products')
+
+        //we must use the filter method
+        //returns only products with the "offer" field as true
+        const onlyOffers = data.filter(product => product.offer)
+
+        setOffers(onlyOffers)
+    }
+
+    loadOffers()
+  },[])
+
+  //for responsiveness to work
+
+  const breakPoints = [
+    { width: 1 , itemsToShow: 1 },
+    { width: 400 , itemsToShow: 2 },
+    { width: 600 , itemsToShow: 3 },
+    { width: 900 , itemsToShow: 4 },
+    { width: 1300 , itemsToShow: 5 },
+  ]
+
+  return (
+    <Container>
+        <Title src={Title_Offers} alt="offers title" />
+
+        <Carousel itemsToShow={3} style={{width: '90%'}} breakPoints={breakPoints}>
+          {
+            //only if categories exists will this method be executed
+            offers && offers.map( product => (
+              <ContainerItems key={product.id}>
+                <Image_Carousel src={product.url} alt="product photo"/>
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+                <Button>Peça agora</Button>
+              </ContainerItems>
+            ))
+          }
+        </Carousel>
+    </Container>
+  )
+}
+
+export default OffersCarousel
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Container,HomeImg } from './styles'
 import HomeImage from '../../assets/Home_Img.svg'
 import CategoryCarousel from '../../components/CategoryCarousel'
+import OffersCarousel from '../../components/OffersCarousel'
 
 function index() {
   return (
     <Container>
         <HomeImg src={HomeImage} alt="Home Image"/>
         <CategoryCarousel/>
+        <OffersCarousel/>
     </Container>
   )
 }
